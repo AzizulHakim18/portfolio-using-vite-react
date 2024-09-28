b96a5186c4bd64f05a9a3e865e3a099cad7aec46
AzizulHakim18 <hakimrony18@gamil.com>
2024-09-29 01:03:46 +0600

    3 beginer projects added

--- src/Pages/Project/BeginerProject.jsx
+++ src/Pages/Project/BeginerProject.jsx
@@ -10,6 +10,57 @@ const BeginerProject = () => {
 
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
                 <div className="flex relative justify-center items-center">
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://i.ibb.co.com/6YRhXKn/burger-shop.png" />
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                        <h2 className="tracking-widest text-lg title-font font-medium  mb-1">Portfolio</h2>
+                        <h1 className="title-font font-medium text-indigo-500 mb-3">HTML || CSS || Javascript</h1>
+                        <p className='text-justify'>
+                            <span className='text-sm font-bold text-gray-700'>Key Features: </span>
+                            This project is a modern and responsive personal portfolio website designed for professionals to showcase their skills, experience, and services. Key features include a sleek hamburger menu for easy navigation on mobile devices, responsive design that adapts to different screen sizes, and smooth animations for an engaging user experience. The portfolio highlights sections like About Me, Services, Education, Testimonials, and Contact with clean layout and stylish UI. JavaScript is used to enhance interactivity, while CSS ensures a visually appealing design with seamless transitions. The project is ideal for professionals looking to create a standout online presence.
+                        </p>
+                        <div className='flex justify-between'>
+                            <div>
+                                <Link to="https://github.com/AzizulHakim18/codealpha_portfolio" className='btn btn-outline mx-1'>Client Side</Link>
+                            </div>
+                            <Link to="https://glittery-sherbet-6d33d9.netlify.app/" className='btn btn-outline'>Preview</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex relative justify-center items-center">
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://i.ibb.co.com/bzP21M3/burger-shop-1.png" />
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                        <h2 className="tracking-widest text-lg title-font font-medium  mb-1">Photon</h2>
+                        <h1 className="title-font font-medium text-indigo-500 mb-3">HTML || CSS || Javascript</h1>
+                        <p className='text-justify'>
+                            <span className='text-sm font-bold text-gray-700'>Key Features: </span>
+                            This project is a filterable portfolio website that allows users to browse images categorized into various sections like Landscape, Portrait, Wildlife, Urban, and Macro. It features dynamic filtering buttons that update the displayed content in real-time, offering a smooth user experience. The project uses HTML, CSS, and JavaScript (ES6), with data being loaded dynamically from a JSON file. Each category button has an active state for visual feedback, improving user interaction. The design is fully responsive, ensuring it looks great on all devices, with added hover animations for a modern touch.The project is ideal for professionals looking to create a standout online presence.
+                        </p>
+                        <div className='flex justify-between'>
+                            <div>
+                                <Link to="https://github.com/AzizulHakim18/CodeAlpha_photon" className='btn btn-outline mx-1'>Client Side</Link>
+                            </div>
+                            <Link to="https://lively-bunny-dbc48c.netlify.app/" className='btn btn-outline'>Preview</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex relative justify-center items-center">
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://i.ibb.co.com/WGbrFTk/burger-shop-2.png" />
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                        <h2 className="tracking-widest text-lg title-font font-medium  mb-1">Calculator</h2>
+                        <h1 className="title-font font-medium text-indigo-500 mb-3">HTML || CSS || Javascript</h1>
+                        <p className='text-justify'>
+                            <span className='text-sm font-bold text-gray-700'>Key Features: </span>
+                            The Fancy Calculator project is a stylish, fully functional calculator that performs basic arithmetic operations like addition, subtraction, multiplication, and division. It also includes advanced features like percentage and square root calculations, along with memory functions (M+ and MR). The calculator boasts a modern, responsive design with smooth animations and hover effects, making it user-friendly across all devices. With error handling for invalid operations and a sleek UI, this project is perfect for daily use, learning web development, and showcasing front-end skills.
+                        </p>
+                        <div className='flex justify-between'>
+                            <div>
+                                <Link to="https://github.com/AzizulHakim18/codeAlpha_calculator" className='btn btn-outline mx-1'>Client Side</Link>
+                            </div>
+                            <Link to="https://ornate-dodol-1a9a14.netlify.app/" className='btn btn-outline'>Preview</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex relative justify-center items-center">
                     <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://i.ibb.co.com/5KQPF6s/burger-shop-4.png" />
                     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                         <h2 className="tracking-widest text-lg title-font font-medium  mb-1">Countdown</h2>
